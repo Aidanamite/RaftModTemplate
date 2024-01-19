454826df8ccb097e0aa37680420e4dbeed5d1b3f
Aidanamite <aid.dav.rei@gmail.com>
2024-01-19 17:24:03 +1300

    Add project files.

---
+++ RaftModTemplate/Main.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using System;
+using Object = UnityEngine.Object;
+using Debug = UnityEngine.Debug;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine.UI;
+using HMLLibrary;
+using System.Linq;
+using Steamworks;
+using System.IO;
+using RaftModLoader;
+
+
+namespace RaftModTemplate
+{
+    public class Main : Mod
+    {
+        public void Start()
+        {
+            Log("Mod has been loaded!");
+        }
+
+        public void OnModUnload()
+        {
+            Log("Mod has been unloaded!");
+        }
+    }
+}
